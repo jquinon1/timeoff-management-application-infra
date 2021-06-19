48ef83391558f6438fa1e0cb35d4815f3ac1fa09
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2021-06-19 16:42:03 -0500

    added ecr output

--- terraform/aws-ecr/ecr.tf
+++ terraform/aws-ecr/ecr.tf
@@ -1,7 +1,11 @@
 resource "aws_ecr_repository" "repository" {
-    for_each = {for repo in var.repositories: repo.name => repo}
+    for_each = {for repo in var.repositories: "${var.environment}-${repo.name}" => repo}
     name = "${var.environment}-${each.key}"
     image_scanning_configuration {
         scan_on_push = each.value.scan_on_push
     }
+}
+
+output "registries" {
+    value = "${aws_ecr_repository.repository}"
 }

89b86c4a69577aad62e0bbcf3d84bb8487e4adee
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2021-06-19 18:35:47 -0500

    fixed typo in the ecr name

--- terraform/aws-ecr/ecr.tf
+++ terraform/aws-ecr/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "repository" {
     for_each = {for repo in var.repositories: "${var.environment}-${repo.name}" => repo}
-    name = "${var.environment}-${each.key}"
+    name = "${each.key}"
     image_scanning_configuration {
         scan_on_push = each.value.scan_on_push
     }

bee8b64dd379902ab2c9f42df8262f3c977bb438
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2021-06-19 18:35:22 -0500

    Modified health check for the tg

--- terraform/aws-alb/main.tf
+++ terraform/aws-alb/main.tf
@@ -45,6 +45,17 @@ module "alb" {
 			backend_protocol = "HTTP"
 			backend_port     = var.target_group_backend_port
 			target_type      = "ip"
+			health_check = {
+      		  enabled             = true
+      		  interval            = 30
+      		  path                = "/"
+      		  port                = "traffic-port"
+      		  healthy_threshold   = 3
+      		  unhealthy_threshold = 3
+      		  timeout             = 6
+      		  protocol            = "HTTP"
+      		  matcher             = "200-399"
+      		}
 		}
 	]   
 	http_tcp_listeners = [
@@ -65,4 +76,16 @@ module "alb" {
 	tags = {
 		Name = "${var.environment}-${var.project_name}-alb"
 	}
+}
+
+output "alb_arn" {
+	value = module.alb.this_lb_id
+}
+
+output "alb_dns_name" {
+	value = module.alb.this_lb_dns_name
+}
+
+output "alb_target_gruops_arns" {
+	value = module.alb.target_group_arns
 }
